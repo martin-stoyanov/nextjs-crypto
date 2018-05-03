efc2b0f5bdf20298b880567a3b48670f7404864d
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-05-03 15:06:28 -0400

    added router.push , redirect, and error checking

--- pages/prices.js
+++ pages/prices.js
@@ -2,6 +2,7 @@ import { Box, Paragraph } from 'grommet';
 import { Card, Select } from 'grommet-controls';
 import { CardTitle, CardContent } from 'grommet-controls/components/Card';
 import fetch from 'isomorphic-unfetch';
+import Router from 'next/router';
 import SinglePrice from '../components/singlePrice';
 import HistoricalPrices from '../components/historicalprices';
 import Layout from '../components/layout';
@@ -19,25 +20,33 @@ class Prices extends React.Component {
       currency: currency.toUpperCase(),
     };
   }
+  onChangeCurrency = (event) => {
+    // link to prices/currency
+    console.log(event.value);
+    console.log(`/prices/${event.value}`);
+    Router.push(`/prices/${event.value}`);
+    // Router.push(`/prices/${event.value}`);
+  }
   render() {
     const { currency } = this.props;
-    console.log(this.props);
     return (
       <Layout title='Bitprice'>
-        <Card size={{ width: 'xlarge' }}>
-          <CardTitle border='bottom'>Welcome to BitPrice!</CardTitle>
-          <CardContent pad='small'>
-            <SinglePrice currency={currency} bpi={this.props.btc} />
-            <Box direction='row' justify='left'>
-              <Paragraph>Currency: &nbsp;&nbsp;</Paragraph>
-              <Select
-                options={currencies}
-                value={currency}
-                onChange={this.onChangeCurrency}
-              />
-            </Box>
-          </CardContent>
-        </Card>
+        <Box direction='column' align='center'>
+          <Card size={{ width: 'xlarge' }}>
+            <CardTitle border='bottom'>Welcome to BitPrice!</CardTitle>
+            <CardContent pad='small'>
+              <SinglePrice currency={currency} bpi={this.props.btc} />
+              <Box direction='row' justify='left'>
+                <Paragraph>Currency: &nbsp;&nbsp;</Paragraph>
+                <Select
+                  options={currencies}
+                  value={currency}
+                  onChange={this.onChangeCurrency}
+                />
+              </Box>
+            </CardContent>
+          </Card>
+        </Box>
         <Box direction='column' align='center'>
           <Card size={{ width: 'xlarge' }}>
             <CardTitle border='bottom'>Historical Prices</CardTitle>
--- server.js
+++ server.js
@@ -9,9 +9,22 @@ const handle = app.getRequestHandler();
 app.prepare()
   .then(() => {
     const server = express();
-
-    server.get('/prices/:currency', (req, res) => app.render(req, res, '/prices', { currency: req.params.currency }));
-
+    // list of currencues
+    const currencies = ['usd', 'eur', 'gbp'];
+    server.get('/prices/:currency', (req, res) => {
+      // check if not in list
+      if (!currencies.includes(req.params.currency.toLowerCase())) {
+        console.log('unavailable currency');
+        res.redirect('/prices/usd');
+      } else {
+        app.render(req, res, '/prices', { currency: req.params.currency });
+        console.log('hello there');
+        console.log(`req: ${req.params.currency}; res: ${res}`);
+      }
+    });
+    server.get('/prices', (req, res) => {
+      res.redirect('/prices/usd');
+    });
     server.get('*', (req, res) => handle(req, res));
 
     server.listen(port, (err) => {
