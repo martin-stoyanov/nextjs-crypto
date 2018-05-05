f6d8ecc820743dc14bbe44fb701dc0ba953f65c7
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-05-05 14:10:08 -0400

    refresh price on interval

--- components/singlePrice.js
+++ components/singlePrice.js
@@ -1,12 +1,44 @@
 import { Paragraph, Box } from 'grommet';
 import React from 'react';
 
-const SinglePrice = props => (
-  <Box direction='column'>
-    <Paragraph>BTC price for {props.bpi.bpi[props.currency].code}:
-      <b> {props.bpi.bpi[props.currency].rate}</b>
-    </Paragraph>
-    <Paragraph>Updated on: <b>{props.bpi.time.updated}</b></Paragraph>
-  </Box>
-);
+// make into a react component- then put timer
+// make componentdidmount with timer, when data received, put in state
+// if new currency, stop new timer, start newcomponent
+class SinglePrice extends React.Component {
+  constructor(props) {
+    super(props);
+    const { bpi, currency } = props;
+    this.state = {
+      price: bpi.bpi[currency].rate,
+      time: bpi.time.updated,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(this.getPrice, 60000);
+  }
+
+  getPrice = async () => {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+    const price = data.bpi[this.props.currency].rate;
+    const time = data.time.updated;
+    this.setState({
+      price,
+      time,
+    });
+  }
+  render() {
+    const { currency } = this.props;
+    const { price, time } = this.state;
+    return (
+      <Box>
+        <Paragraph>{`BTC price for ${currency}: `}
+          <b>{price}</b>
+        </Paragraph>
+        <Paragraph>Updated on: <b>{time}</b></Paragraph>
+      </Box>
+    );
+  }
+}
 export default SinglePrice;
